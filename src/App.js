f09fd8e9eac87a137669cb6c08dbdb67fde35e79
Nitesh Saini <31733@exchange.isb.edu>
2024-04-16 16:21:32 +0530

    added analytics

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Header/Header'
 import Home from './Components/Home/Home';
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-WEDMK83T4H");
+ReactGA.send({ hitType: "pageview", page: document.location.pathname, title: "Custom Title" });
 
 function App() {
   return (
